aba6692ee340495171a645a8c906a40d46a34b08
Maharshi <maharshilavana@gmail.com>
2026-09-23 18:17:45 +0530

    replaced recursive text splitter with semantic chunker

--- src/video_summarizer/services/ingestion.py
+++ src/video_summarizer/services/ingestion.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 from langchain_community.document_loaders import YoutubeLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_experimental.text_splitter import SemanticChunker
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_pinecone import PineconeVectorStore
 import re
@@ -28,9 +29,10 @@ def ingest_video(url : str , video_id : str):
 
     data = loader.load()
     # title = data[0].metadata.get("title", "Untitled") if data else "Untitled"
-    splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 1000,
-        chunk_overlap = 200
+    splitter = SemanticChunker(
+        embeddings = embedding_model,
+        breakpoint_threshold_type= "percentile",
+        breakpoint_threshold_amount=95
     )
 
     chunks = splitter.split_documents(data)
